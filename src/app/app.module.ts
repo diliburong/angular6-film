b9c90e9943cd03269317b3de745db36ca67c982a
diliburong <diliburong@126.com>
2018-07-28 22:09:40 +0800

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
-    // LazyLoadImageModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
