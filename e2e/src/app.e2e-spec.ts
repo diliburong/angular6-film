a2db243f907ff0009af137fd40f38a8610973ef5
diliburong <diliburong@126.com>
2018-07-29 18:04:13 +0800

    finish navbar

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to my-app!');
+    page.navigateTo(); 
+    expect(page.getParagraphText()).toEqual('Welcome to angular6-film!');
   });
 });
